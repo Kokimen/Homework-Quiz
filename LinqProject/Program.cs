c47beb4335ac3d20b75d89aeeffa46d504076cef
Kokimen <dev.burakkoktay@outlook.com>
2021-02-27 00:52:32 +0300

    11th

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -17,24 +17,92 @@ namespace LinqProject
             List<Product> products = new List<Product>
             {
                 new Product{ProductId=1,CategoryId=1,ProductName="Acer Laptop",QuantityPerUnit="32 GB Ram", UnitPrice=10000,UnitsInStock=5},
-                new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 GB Ram", UnitPrice=8000,UnitsInStock=3},
-                new Product{ProductId=3,CategoryId=1,ProductName="HP Laptop",QuantityPerUnit="8 GB Ram", UnitPrice=6000,UnitsInStock=2},
+                new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 GB Ram", UnitPrice=18000,UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=1,ProductName="HP Laptop",QuantityPerUnit="8 GB Ram", UnitPrice=18000,UnitsInStock=2},
                 new Product{ProductId=4,CategoryId=2,ProductName="Samsung Telefon",QuantityPerUnit="4 GB Ram", UnitPrice=5000,UnitsInStock=15},
                 new Product{ProductId=5,CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit="3 GB Ram", UnitPrice=7000,UnitsInStock=0},
             };
 
-            var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3);
+            //AnyTest(products);
+
+            //FindTest(products);
+
+            //FindAllTest(products);
+
+            //TestForEach(products);
+
+            //AscDescTest(products);
+
+            //ClassicLinqTest(products);
+
+            var result = from p in products
+                         join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+
+                foreach (var productDto in result)
+                {
+                    Console.WriteLine(productDto.ProductName + " " + productDto.CategoryName);
+                }
+
+        }
+
+        private static void ClassicLinqTest(List<Product> products)
+        {
+            var result = from p in products
+                         where p.UnitPrice > 3000
+                         orderby p.UnitPrice descending
+                         select p;
 
             foreach (var product in result)
             {
                 Console.WriteLine(product.ProductName);
             }
-
-            
         }
 
-        
-       
+        private static void AscDescTest(List<Product> products)
+        {
+            var result = products.Where(p => p.ProductName.Contains("A")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void TestForEach(List<Product> products)
+        {
+            var result = products.Where(p => p.UnitPrice > 2000 && p.UnitsInStock > 3);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products)
+        {
+            var result = products.FindAll(p => p.ProductName.Contains("top"));
+            Console.WriteLine(result);
+        }
+
+        private static void FindTest(List<Product> products)
+        {
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result.ProductName);
+        }
+
+        private static void AnyTest(List<Product> products)
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop"); //any bool döner, true false var mı yok mu
+            Console.WriteLine(result);
+        }
+
+    }
+    class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string CategoryName{ get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
     }
     class Product
     {
---
+++ Reflection/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DortIslem dortIslem=new DortIslem(2,3);
+            //Console.WriteLine(dortIslem.Topla2());
+            //Console.WriteLine(dortIslem.Topla(3,4));
+
+            var type = typeof(DortIslem);
+
+            //DortIslem dortIslem = (DortIslem) Activator.CreateInstance(type, 6, 7);
+            //Console.WriteLine(dortIslem.Topla(4,5));
+            //Console.WriteLine(dortIslem.Topla2());
+
+            var instance= Activator.CreateInstance(type, 6, 7);
+            Console.WriteLine(instance.GetType().GetMethod("Topla2").Invoke(instance, null));
+
+            var methods = type.GetMethods();
+
+            foreach (var info in methods)
+            {
+                Console.WriteLine("MethodName: {0}", info.Name);
+
+                foreach (var parameterInfo in info.GetParameters())
+                {
+                    Console.WriteLine("ParameterName : {0}", parameterInfo.Name);
+                }
+
+                foreach (var attribute in info.GetCustomAttributes())
+                {
+                    Console.WriteLine("AttributeName: {0}",attribute.GetType().Name);
+                }
+            }
+
+        }
+    }
+
+    class DortIslem
+    {
+        private int _sayi1;
+        private int _sayi2;
+
+        public DortIslem(int sayi1, int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+        }
+
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1* sayi2;
+        }
+
+        public int Topla2()
+        {
+            return _sayi1 + _sayi2;
+        }
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+    }
+   
+        
+
+    
+}
